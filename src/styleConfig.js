eb34535ac13b98e8253374fe8126a065b2b3206e
gelugu <mkraev@vtb.ru>
2021-06-08 21:43:08 +0300

    add wedding colors

--- src/styleConfig.js
+++ src/styleConfig.js
@@ -1,7 +1,11 @@
 export const COLORS = {
+  his: "#264136",
+  her: "#ccb0ad",
+
+  pink: "#cc85c8",
+  yellow: "#f6cc4b",
+
   dark: "#555b6e",
   grey: "#89b0ae",
-  his: "#bee3db",
   light: "#faf9f9",
-  her: "#ffd6ba",
 }
